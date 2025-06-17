f2ed23185bee41c76c8014d63524ad4b7a725a83
nrf24l01 <s.nevrovsky@ya.ru>
2025-06-17 22:34:26 +0300

    Added gRPC proto

---
+++ grpc_compile.sh
@@ -0,0 +1,4 @@
+rm -rf protos_compiled
+mkdir protos_compiled
+python -m grpc_tools.protoc -I./protos --python_out=./protos_compiled --grpc_python_out=./protos_compiled ./protos/*
+echo "import * from *" >> protos_compiled/__init__.py
