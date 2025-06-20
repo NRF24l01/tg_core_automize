1939ed76352df74b629ea225456285ab08c2bdfc
nrf24l01 <s.nevrovsky@ya.ru>
2025-06-20 19:34:23 +0300

    login logic

--- main.py
+++ main.py
@@ -4,12 +4,31 @@ from threading import Thread
 from config import HOST, PORT
 
 
+
+def process_client(sock: socket_server, l: Logger):
+    controller = SocketController(socket=sock)
+    l.info("Start client processing")
+    client_info = None
+    while not client_info:
+        client_info = controller.read_json()
+    
+    client_name = client_info["name"]
+    client_required_events = client_info["required"]
+    l.info(f"Client with name {client_name} requested events with id: {', '.join(client_required_events)}")
+
 def client_worker(l: Logger):
     sock = socket_server()
     l.info(f"Starting socket server on {HOST}:{PORT}")
     sock.bind((HOST, PORT))
     sock.listen(1)
     
+    l.info("Waiting for connections")
+    while True:
+        conn, addr = sock.accept()
+        l.info(f"Connection from {addr}")
+        t = Thread(target=process_client, args=(conn, l))
+        t.start()
+        
     
 
 if __name__ == "__main__":
