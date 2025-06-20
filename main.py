6069c08b36a518cd75fca7b0f12405b7375b4a4b
nrf24l01 <s.nevrovsky@ya.ru>
2025-06-20 22:42:06 +0300

    created 2side connection example

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from socket import socket as socket_server
 from threading import Thread
 from config import HOST, PORT
 
-
+tasks = {} # dict, keys - client ids, contains fully prepaired messages for client
+clients = set() # list with clients names
 
 def process_client(sock: socket_server, l: Logger):
     controller = SocketController(socket=sock)
@@ -13,8 +14,21 @@ def process_client(sock: socket_server, l: Logger):
         client_info = controller.read_json()
     
     client_name = client_info["name"]
+    clients.add(client_name)
     client_required_events = client_info["required"]
-    l.info(f"Client with name {client_name} requested events with id: {', '.join(client_required_events)}")
+    l.info(f"Client with name {client_name} requested events with id: {', '.join([str(i) for i in client_required_events])}")
+    
+    data = None
+    while True:
+        if controller.data_avalible():
+            data = controller.read_json()
+            print("Recived:", data)
+        if client_name in tasks.keys() and len(tasks[client_name]) != 0:
+            for i, task in enumerate(tasks[client_name]):
+                print(task)
+                if task["type"] in client_required_events:
+                    controller.send_json(task)
+                tasks[client_name].pop(i)
 
 def client_worker(l: Logger):
     sock = socket_server()
@@ -26,11 +40,24 @@ def client_worker(l: Logger):
     while True:
         conn, addr = sock.accept()
         l.info(f"Connection from {addr}")
-        t = Thread(target=process_client, args=(conn, l))
+        t = Thread(target=process_client, args=(conn, l, ))
         t.start()
         
-    
+def test_send():
+    while True:
+        i = input()
+        if i == "":
+            exit()
+        for client in clients:
+            if client in tasks.keys():
+                tasks[client].append({"type": 1, "message": i})
+            else:
+                tasks[client] = [{"type": 1, "message": i}]
 
 if __name__ == "__main__":
     l = Logger()
-    l.log("Starting")
+    l.info("Starting")
+    t = Thread(target=test_send)
+    t.start()
+    
+    client_worker(l)
