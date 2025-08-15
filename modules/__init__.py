9019044fd908eb75f73b150052666200b172775a
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-15 10:39:05 +0300

    Add process_message import to module initialization

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,3 +1,4 @@
 from .socket_controller import AsyncSocketController
 from .logger import Logger
 from .serializer import serialize_sender, extract_chat_id
+from .command_processer import process_message
